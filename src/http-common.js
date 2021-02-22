9b7ae868216e6f13f2d675afa96f7f3e118c7658
Sahil Sharma <sahilsharma@bebotechnologies.com>
2021-02-22 14:47:24 +0530

    changed hiroku url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://react-json-server3546.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
